426bdc2410249c895abe296b3d9e59f2d8ecc2cb
meichflo <meichflo@students.zhaw.ch>
2024-03-09 13:38:43 +0100

    add a loop

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,12 @@
+import time
+
 print("hello world")
+letters = ["F","L","O","R","I","A","N",]
+word= ""
+
+for i in letters:
+    print(i)
+    word = word + str(i)
+    time.sleep(0.2)
+
+print(f"-------\nThe entire word is: {word.capitalize()}")
